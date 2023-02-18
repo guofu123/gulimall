fc37601b42e23a34b25fb3d9aad6b76178f5c6c5
guguofu <3165177264@qq.com>
2023-02-18 21:33:27 +0800

    nacos config 简单使用

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimailCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimailCouponApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.coupon.dao")
@@ -11,7 +12,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GulimailCouponApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimailCouponApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(GulimailCouponApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
     }
 
 }
