b74d52f70d5d4f11b5d26843d7e43432d5649d71
guguofu <3165177264@qq.com>
2023-02-25 23:36:38 +0800

    update test的引用为Junit4

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -2,7 +2,7 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.AttrEntity;
 import com.atguigu.gulimall.product.service.AttrService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
 
     @Autowired
     AttrService attrService;
 
     @Test
-    void contextLoads() {
+    public  void contextLoads() {
 //        AttrEntity entity = new AttrEntity();
 //        entity.setAttrName("计算机");
 //        attrService.save(entity);
