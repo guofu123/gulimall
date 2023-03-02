c14342584edab0cfcce8b6000b1315b314a60eb7
guguofu <3165177264@qq.com>
2023-03-02 21:30:24 +0800

    Java使用Stream编写多级菜单

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -3,8 +3,11 @@ package com.atguigu.gulimall.product.service.impl;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -30,33 +33,60 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     }
 
     /**
-     * 获取多级菜单组合
+     * 使用Stream获取多级菜单组合
      * @return
      */
     @Override
-    public List<CategoryEntity> getCategoryMenu(){
+    public List<CategoryEntity> getCategoryMenu() {
+        List<CategoryEntity> categoryEntities = this.list();
+        List<CategoryEntity> levelV1 = categoryEntities.stream().
+                filter(categoryEntity -> categoryEntity.getParentCid().intValue() == 0)
+                .sorted(Comparator.comparingInt(a -> (a.getSort() == null ? 0 : a.getSort()))).
+                map(category -> {
+                    category.setChildCategory(getChildCategoryMenu(category.getCatId(), categoryEntities));
+                    return category;
+                }).collect(Collectors.toList());
+
+        return levelV1;
+    }
+
+    public List<CategoryEntity> getChildCategoryMenu(Long cid, List<CategoryEntity> categories) {
+        return categories.stream()
+                .filter(category -> category.getParentCid().equals(cid))
+                .sorted(Comparator.comparingInt(a -> (a.getSort() == null ? 0 : a.getSort())))
+                .map(category -> {
+                        category.setChildCategory(getChildCategoryMenu(category.getCatId(), categories));
+                        return category;
+                }).collect(Collectors.toList());
+    }
+
+    /**
+     * 原生方式获取多级菜单
+     * @return
+     */
+    public List<CategoryEntity> getCategoryMenu1() {
         List<CategoryEntity> categories = this.list();
         List<CategoryEntity> levelV1 = new ArrayList<>();
         for (CategoryEntity cate : categories) {
             if (cate.getParentCid().intValue() == 0) {
-                cate.setChildCategory(getChildCategoryMenu(cate.getCatId(), categories));
+                cate.setChildCategory(getChildCategoryMenu1(cate.getCatId(), categories));
                 levelV1.add(cate);
             }
         }
-        return  levelV1;
+        return levelV1;
     }
 
-    public List<CategoryEntity> getChildCategoryMenu(Long cid, List<CategoryEntity> categories){
+    public List<CategoryEntity> getChildCategoryMenu1(Long cid, List<CategoryEntity> categories) {
         ArrayList<CategoryEntity> categoryEntities = new ArrayList<>();
         for (CategoryEntity cate : categories) {
-            if (cate.getParentCid().equals(cid)){
-                cate.setChildCategory(getChildCategoryMenu(cate.getCatId(),categories));
+            if (cate.getParentCid().equals(cid)) {
+                cate.setChildCategory(getChildCategoryMenu1(cate.getCatId(), categories));
                 categoryEntities.add(cate);
             }
         }
-        if (categoryEntities.size()==0){
+        if (categoryEntities.size() == 0) {
             return null;
-        }else {
+        } else {
             return categoryEntities;
         }
     }
